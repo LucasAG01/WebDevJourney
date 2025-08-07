b8d5833a4f05ee0abb7251b371cd46ec9cf83e6b
lucas <lucas.alcobia01@gmail.com>
2025-08-07 20:53:28 +0200

    ✨ mas input / const 💬 Accept user input - Aprendí a recibir entrada de usuarios con prompt() y con html con inputs y buttons 💱 Type conversion - Practiqué conversión entre tipos de datos (string , número, boolean) 🚫 Constants - Entendí el uso de const para valores inmutables
    ⭐ Como aplicación práctica, creé un:
    🔢 Counter program

--- JsFiles/jsindex.js
+++ JsFiles/jsindex.js
@@ -16,10 +16,10 @@ se comporta como si fuera el valor que contiene.
 1. declaracion let x;
 2. asignación x = 100;
 */ 
-let x;
-x = 100;
+//let x;
+//x = 100;
 
-let age = 25;
+//let age = 25;
 let price = 10.99;
 let gpa = 2.1;
 
@@ -77,17 +77,7 @@ let result = 1 + 2 * 3 + 4 ** 2
 // 1 + 6 + 16
 // 7 + 16
 // 23
-
-console.log(result);
-
-
-
-
-
-
-
-
-
+//console.log(result);
 
 /*
 console.log(typeof firstName);
@@ -100,3 +90,128 @@ console.log(`You are ${age} years old`);
 console.log(`The price is ${price}€`);
 console.log(`Your gpa is: ${gpa}`);
 */
+
+
+/*Aceptar User Prompt*/
+//Forma fácil, windwos promt
+//let username;
+//username = window.prompt("Whats your username"); 
+//console.log(username);
+
+//La forma buena con una htmlBox
+let username;
+let check;
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    
+    check=Boolean(username);
+    
+    if(check){
+        document.getElementById("myH2").textContent = `Your name is ${firstName}`; 
+    }
+    else{
+        document.getElementById("myH2").textContent = `You put nothing`; 
+    }   
+    //console.log(`${username}, datatype: ${typeof username}`);
+}
+
+
+/* Type Conversion */
+
+let age;
+
+age = Number(age); //convierte lo que le pongas a un numero
+
+age+1;
+
+
+//console.log(`${age}, datatype: ${typeof age}`);
+
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x); //NaN not a number
+y = String(y);
+z = Boolean(z); //Simpre que z tenga un valor, simepre sra true si esta vacio es false
+//console.log(x, typeof x);
+//console.log(y, typeof y);
+//console.log(z, typeof z); 
+
+
+
+/*COnstantes usando calculo de circunferencia*/
+
+let pi= Math.PI; //3 digitos
+//pero para otras cosas que si o ni nadie durante el rpograma pueda cambiar es asi
+const PI = 3.14159;
+
+//radious = window.prompt("Dame el radio del círculo: ");
+
+//todo esto va dentro de function(){aqui}
+
+document.getElementById("submitRadio").addEventListener("click",function(){
+/*declaraciones de inputs y resultado*/     
+const radioInput = document.getElementById("radioInput").value;
+const resultadoElement = document.getElementById("Resultado");
+
+if(radioInput.trim() === "" || isNaN(radioInput)){
+    resultadoElement.textContent = "Debes ingresar campo válido, pista: numero";
+
+}else{
+
+    const radio = Number(radioInput);
+
+    if(radioInput < 0){
+        resultadoElement.textContent = "Debes ingresar numero mayor a 0";
+
+    }else{
+        const circunferencia = 2*pi*radio;
+
+        resultadoElement.textContent = `La circunferencia es con radio ${radioInput} es: ${circunferencia.toFixed(4)} cm`;
+    }
+}
+});
+
+
+
+//radious=Number(radious);
+
+//circunferencia = 2*pi*radious;
+//console.log(circunferencia.toFixed(2));
+
+
+/*CONTADOR*/ 
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.addEventListener("click",function(){  
+    count++;
+    countLabel.textContent = count;
+})
+
+resetBtn.addEventListener("click", function(){
+    count = 0;
+    countLabel.textContent = count;
+})
+
+decreaseBtn.addEventListener("click",function(){
+    count--;
+    countLabel.textContent = count;
+})
+
+
+
+
+
+
+
+
+
+
+
