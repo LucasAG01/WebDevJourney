fd7b7e3208b859310a263e2d3a6d678302d86b32
LucasAG01 <lucas.alcobia01@gmail.com>
2025-09-05 21:35:04 +0200

    ⭐ Weather App project ☀️

--- JsFiles/jsindex.js
+++ JsFiles/jsindex.js
@@ -2497,5 +2497,135 @@ async function fetchData(){
 }
 
 
+/* WEATHER APP */
+
+
+const weatherForm = document.querySelector(".weatherForm"); //QuerySelector devuelve el primer eleemnto en esa clase.
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "44ea31cb99fe513d29059fd1589bfbbc";
+
+//Cuando le des al boton, harás un submit, despues haremos el siguiente código (event)
+weatherForm.addEventListener("submit", async event =>{
+
+    event.preventDefault(); //los submit en forms, tienen a refrescar la págino, con esto lo evitamos
+
+    const city = cityInput.value;
+
+    if(city){  
+        try{
+            const weatherData = await getWeatherData(city);
+            dispplayWeatherInfo(weatherData);
+
+        }catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("Please enter a city");
+    }
+});
+
+async function getWeatherData(city){
+
+    //https://openweathermap.org/current
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    if(!response.ok){
+        throw new Error("COuld not fetch weather data");
+    }
+
+    return await response.json();
+}
+
+function dispplayWeatherInfo(data){
+        //console.log(data)
+        ///creamos un objeto donde almacenamos lo que nos interesa usar. {} significa object{cosas} 
+        // weather es un array de objetos
+        //data es un objeto gigante, con objetos anidados
+        const {name: city, 
+               main:{temp, humidity}, 
+               weather: [{description, id}]} = data;
+
+        //Ahora que tenemos datos, vemos a mostrar la card
+        card.textContent ="";
+        card.style.display ="flex"  
+
+        //crear las constant
+        const cityDisplay = document.createElement("h1")
+        const tempDisplay = document.createElement("p");
+        const humidityDisplay = document.createElement("p");
+        const descDisplay = document.createElement("p");
+        const weatherEmoji = document.createElement("p");
+        const errorDisplay = document.createElement("p");
+
+        //poner el textto de la variable en su sitio
+        cityDisplay.textContent = city;
+        tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
+        humidityDisplay.textContent = `Humidity: ${humidity}%`;
+        descDisplay.textContent= description;
+        weatherEmoji.textContent = getWeatherEmoji(id);
+        errorDisplay.textContent = displayError();
+
+        
+        //Aplicar el css
+        cityDisplay.classList.add("cityDisplay");
+        tempDisplay.classList.add("tempDisplay");
+        humidityDisplay.classList.add("humidityDisplay");
+        descDisplay.classList.add("descDisplay");
+        weatherEmoji.classList.add("weatherEmoji");
+        errorDisplay.classList.add("errorDisplay");
+
+        //Añadirla a la card    
+        card.appendChild(cityDisplay);
+        card.appendChild(tempDisplay);
+        card.appendChild(humidityDisplay);
+        card.appendChild(descDisplay);
+        card.appendChild(weatherEmoji);
+        card.appendChild(errorDisplay);
+
+}
+
+function getWeatherEmoji(weatherId){
+
+    switch(true){
+        case(weatherId >= 200 && weatherId < 300):
+            return "⛈️";
+        case(weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case(weatherId >= 500 && weatherId < 600):
+            return "🌧️";
+        case(weatherId >= 600 && weatherId < 700):
+            return "🌨️";
+        case(weatherId >= 700 && weatherId < 800):
+            return "🌫️";
+        case(weatherId === 800):
+            return "☀️";
+        case(weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "⁉️";
+    }
+}
+
+function displayError(message){
+
+    //Creamos un p y le aplicamos el css de error
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent ="";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
+
+
+//🌩️☀️🌪️☁️🌧️🌦️⛈️🌨️⛅🌥️
+
+
 
 
